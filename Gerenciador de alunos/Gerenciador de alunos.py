66dc349c945019eefc9193dc256d70c29a52263a
KevBoyz <71911085+KevBoyz@users.noreply.github.com>
2020-12-21 23:29:16 -0300

    (New Function) Version 1.3 to 1.4

--- Gerenciador de alunos/Gerenciador de alunos.py
+++ Gerenciador de alunos/Gerenciador de alunos.py
@@ -5,17 +5,18 @@ done = False
 s = 0
 
 print('''-=-=-=-=- Gerenciador de Alunos -=-=-=-=-
-Programado por KevBoyz >>> Versão   1.1
+Programado por KevBoyz >>> Versão   1.4
 ''')
 
 
 while True:
     print('''Menu de opções:
 _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_/ /
-[1] conferir lista de alunos    |
-[2] cadastrar novo aluno        |
-[3] cadastrar nova turma        |
-[4] editar configurações do aluno''')
+[1] Conferir lista de alunos    |
+[2] Cadastrar novo aluno        |
+[3] Cadastrar nova turma        |
+[4] Configurações do programa   |
+[5] editar configurações do aluno''')
     inpt = int(input('>>> '))
     print()
     done = False
@@ -74,74 +75,103 @@ _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_/ /
         print('''Turma criada com Sucesso!
         ''')
 
-
     elif inpt == 4:
+        while True:
+            print('''=== Configurações do Programa ===
+            
+[1] Deletar turmas
+[2] Deletar alunos
+[3] Voltar ao programa
+''')
+            inpt = int(input('>>> '))
+
+            if inpt == 1:
+                print('Turmas disponiveis: ', end='')
+                for turma in turmas:
+                    print(f'{turma}', end='')
+                del_item = str(input('Digite o nome da turma a ser deletada: '))
+                if del_item not in turmas:
+                    print('Turma não encontrada')
+                    del_item = int(input('Digite o nome da turma a ser deletada: '))
+                elif del_item in turmas:
+                    turmas.remove(del_item)
+                    print(turmas)
+                    for aluno in geral_list:
+                        if aluno[2] == del_item:
+                            aluno[2] = 'Aluno sem Turma'
+                    print('Concluido com exito')
+                    print()
+            elif inpt == 2:
+                name = str(input('Digite o nome do aluno a ser deletado: '))
+                if name not in geral_list:
+                    print('Aluno não encontrado')
+                    name = str(input('Digite o nome do aluno a ser deletado: '))
+                elif name in geral_list:
+                    for aluno in geral_list:
+                        if aluno[0] == name:
+                            geral_list.remove(aluno[2])
+                            geral_list.remove(aluno[1])
+                            geral_list.remove(aluno[0])
+                            print('Concluido com exito')
+                            print()
+            elif inpt == 3:
+                break
+                print()
+
+
+
+    elif inpt == 5:
         if len(geral_list) == 0 or len(turmas) == 0:
             print('Você ainda não possui alunos cadastrados')
             print()
         else:
             print('=== Configurações do aluno ===')
-            print('Turmas disponíveis: ', end='')
-            for turma in turmas:
-                print(turma, end=', ')
-            print(' ... Fim da lista', end='')
-            find1 = str(input('\nDigite a turma do aluno desejado: '))
-            if find1 not in turmas:
-                while find1 not in turmas:
-                    print('Turma não encontrada... ')
-                    find1 = str(input('Digite a turma do aluno desejado: '))
-            else:
-                print('Turma encontrada... Selecione um aluno para atualizar a configuração')
-                print('-=-'*20)
-                for aluno in geral_list:
-                    if aluno[2] == find1:
-                        print(f'{s + 1}. {aluno[0]}')
-                print()
-                name = str(input('Digite o nome do aluno '))
-                for aluno in geral_list:
-                    if aluno[0] == name:
-                        done = True
-                    if done == True:
-                        print('... Aluno encontrado!')
-                        print('-=-'*20)
-                        for aluno in geral_list:
-                            if aluno[2] == find1:
-                                print('Dados atuais do aluno:')
-                                print(f'Nome: {aluno[0]:<20} | Data de nascimento: {aluno[1]}')
-                                print('-=-'*20)
-                        while True:
-                            print('''Selecione o processo desejado;
+            print()
+            name = str(input('Digite o nome do aluno '))
+            for aluno in geral_list:
+                if aluno[0] == name:
+                    done = True
+                if done == True:
+                    print('... Aluno encontrado!')
+                    print('-=-'*20)
+                    for aluno in geral_list:
+                        if aluno[0] == name:
+                            print('Dados atuais do aluno:')
+                            print(f'Nome: {aluno[0]:<20} | Data de nascimento: {aluno[1]}')
+                            print('-=-'*20)
+                    while True:
+                        print('''Selecione o processo desejado;
 [1] Trocar nome do aluno
 [2] Trocar data de nascimento 
 [3] Mudar aluno de turma
 [4] Voltar ao programa''')
-                            inpt = int(input('>>> '))
-                            if inpt == 1:
-                                new_name = str(input('Digite um novo nome: '))
-                                for aluno in geral_list:
-                                    if aluno[0] == name:
-                                        aluno[0] = new_name
-                                print('Concluido com exito')
-                                print()
-                            elif inpt == 2:
-                                new_age = str(input('Nova data de nascimento: '))
-                                for aluno in geral_list:
-                                    if aluno[0] == name:
-                                        aluno[1] = new_age
-                                print('Concluido com exito')
-                                print()
-                            elif inpt == 3:
-                                name = str(input('Nome do aluno a ser mudado de turma: '))
-                                for aluno in geral_list:
-                                    if aluno[0] == name:
-                                       done = True
-                                    if done == False:
-                                        while done == False:
-                                            print('Aluno não encontrado...')
-                                            name = str(input('Nome do aluno a ser mudado de turma: '))
-                                            for aluno in geral_list:
-                                                if aluno[0] == name:
-                                                    done = True
+                        inpt = int(input('>>> '))
+                        if inpt == 1:
+                            new_name = str(input('Digite um novo nome: '))
+                            for aluno in geral_list:
+                                if aluno[0] == name:
+                                    aluno[0] = new_name
+                            print('Concluido com exito')
+                            print()
+                        elif inpt == 2:
+                            new_age = str(input('Nova data de nascimento: '))
+                            for aluno in geral_list:
+                                if aluno[0] == name:
+                                    aluno[1] = new_age
+                            print('Concluido com exito')
+                            print()
+                        elif inpt == 3:
+                            name = str(input('Nome do aluno a ser mudado de turma: '))
+                            for aluno in geral_list:
+                                if aluno[0] == name:
+                                    done = True
+                                if done == False:
+                                    while done == False:
+                                        print('Aluno não encontrado...')
+                                        name = str(input('Nome do aluno a ser mudado de turma: '))
+                                        for aluno in geral_list:
+                                            if aluno[0] == name:
+                                                done = True
                                 else:
                                     for aluno in geral_list:
                                         if aluno[0] == name:
@@ -156,7 +186,7 @@ _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_/ /
                                                 aluno[2] = new_turm
                                     print('Concluido com exito')
                                     print()
-                            elif inpt == 4:
-                                print('Saindo do configurador...')
-                                print()
-                                break
+                        elif inpt == 4:
+                            print('Saindo do configurador...')
+                            print()
+                            break
